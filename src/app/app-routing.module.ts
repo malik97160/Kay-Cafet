30148494735b74b957691751267b3b3728d6d201
couchy malik <malik.couchy@gmail.com>
2020-09-20 00:08:04 +0200

    feat: sidebar + footer +header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { DashboadComponent } from './modules/dashboad/dashboad.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: AdminComponent,
+  children: [{
+    path: '', 
+    component: DashboadComponent
+  }, {
+    path: 'posts',
+    component: PostsComponent
+  }
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layouts/admin/admin.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { DashboadComponent } from 'src/app/modules/dashboad/dashboad.component';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { AdminComponent } from './admin.component';
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    DashboadComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule
+  ]
+})
+export class AdminModule { }
